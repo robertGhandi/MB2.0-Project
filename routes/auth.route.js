55e763cfa6496dcfc58a27c7328f26e585be3862
robertGhandi <robertzerah@gmail.com>
2024-06-20 02:34:56 +0100

    removed auth.route.js

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const { registerUser, loginUser } = require("../controllers/auth.controller");
-const { validateSignUp, validateLogin } = require("../validators/auth.validator");
-const express = require("express");
-const router = express.Router();
-
-router.post("/register", validateSignUp, registerUser);
-router.post("/login", validateLogin, loginUser);
-
-module.exports = router;
